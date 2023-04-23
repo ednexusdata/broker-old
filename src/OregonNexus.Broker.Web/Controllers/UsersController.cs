08d42f027d5c64789b4b47818e5f784e8d4a78e4
Makoa Jacobsen <makoa@makoajacobsen.com>
2023-04-23 18:09:44 +0000

    Implement validation logic with view model.

--- src/OregonNexus.Broker.Web/Controllers/UsersController.cs
+++ src/OregonNexus.Broker.Web/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using OregonNexus.Broker.Data;
 using OregonNexus.Broker.Domain;
 using OregonNexus.Broker.SharedKernel;
+using OregonNexus.Broker.Web.Models;
 
 namespace OregonNexus.Broker.Web.Controllers;
 
@@ -26,21 +27,28 @@ public class UsersController : Controller
 
     public async Task<IActionResult> Index()
     {
-        var combinedUsers = new List<CombinedUser>();
+        var combinedUsers = new List<UserViewModel>();
         
         var identityUsers = await _db.Users.OrderBy(x => x.NormalizedEmail).ToListAsync();
         var users = await _repo.ListAsync();
 
         foreach(var identityUser in identityUsers)
         {
-            var combinedUser = new CombinedUser();
-            combinedUser.IdentityUser = identityUser;
-            combinedUser.ApplicationUser = users.Where(x => x.Id == identityUser.Id).FirstOrDefault();
+            var appUser = users.Where(x => x.Id == identityUser.Id).FirstOrDefault();
+
+            var combinedUser = new UserViewModel()
+            {
+                UserId = identityUser.Id,
+                FirstName = appUser.FirstName,
+                LastName = appUser.LastName,
+                IsSuperAdmin = appUser.IsSuperAdmin,
+                Email = identityUser.Email
+            };
 
             combinedUsers.Add(combinedUser);
         }
 
-        combinedUsers.OrderBy(x => x.ApplicationUser?.LastName).OrderBy(x => x.ApplicationUser?.FirstName);
+        combinedUsers.OrderBy(x => x.LastName).OrderBy(x => x.FirstName);
 
         return View(combinedUsers);
     }
@@ -52,17 +60,19 @@ public class UsersController : Controller
 
     [ValidateAntiForgeryToken]
     [HttpPost]
-    public async Task<IActionResult> Create(CombinedUser data)
+    public async Task<IActionResult> Create(UserViewModel data)
     {
-        var identityUser = new IdentityUser<Guid> { UserName = data.IdentityUser.Email, Email = data.IdentityUser.Email }; 
+        if (!ModelState.IsValid) { return View("Add"); }
+        
+        var identityUser = new IdentityUser<Guid> { UserName = data.Email, Email = data.Email }; 
         var result = await _userManager.CreateAsync(identityUser);
 
         var user = new User()
         {
             Id = identityUser.Id,
-            FirstName = data.ApplicationUser.FirstName,
-            LastName = data.ApplicationUser.LastName,
-            IsSuperAdmin = data.ApplicationUser.IsSuperAdmin
+            FirstName = data.FirstName,
+            LastName = data.LastName,
+            IsSuperAdmin = data.IsSuperAdmin
         };
 
         await _repo.AddAsync(user);
@@ -72,40 +82,63 @@ public class UsersController : Controller
 
     public async Task<IActionResult> Edit(Guid Id)
     {
-        var combinedUser = new CombinedUser();
+        var identityUser = await _db.Users.Where(x => x.Id == Id).FirstOrDefaultAsync();
+        var applicationUser = await _repo.GetByIdAsync(Id);
 
-        combinedUser.IdentityUser = await _db.Users.Where(x => x.Id == Id).FirstOrDefaultAsync();
-        combinedUser.ApplicationUser = await _repo.GetByIdAsync(Id);
+        var userViewModel = new UserViewModel();
 
-        return View(combinedUser);
+        if (applicationUser is not null && identityUser is not null)
+        {
+            userViewModel = new UserViewModel()
+            {
+                UserId = applicationUser.Id,
+                IdentityUser = identityUser,
+                FirstName = applicationUser.FirstName,
+                LastName = applicationUser.LastName,
+                IsSuperAdmin = applicationUser.IsSuperAdmin,
+                Email = identityUser.Email
+            };
+        }
+
+        return View(userViewModel);
     }
 
     [ValidateAntiForgeryToken]
     [HttpPatch]
-    public async Task<IActionResult> Update(CombinedUser data)
+    public async Task<IActionResult> Update(UserViewModel data)
     {
         // Get existing user
-        var user = await _userManager.FindByIdAsync(data.IdentityUser.Id.ToString());
+        var user = await _userManager.FindByIdAsync(data.UserId.ToString());
 
-        if (data.IdentityUser.Email != user.Email)
+        if (user is null) { throw new ArgumentException("Not a valid user."); }
+
+        if (!ModelState.IsValid) { return View("Edit"); }
+
+        if (data.Email != user.Email)
         {
             // Update email
-            var token = await _userManager.GenerateChangeEmailTokenAsync(user, data.IdentityUser.Email);
-            var result = await _userManager.ChangeEmailAsync(user, data.IdentityUser.Email, token);
+            var token = await _userManager.GenerateChangeEmailTokenAsync(user, data.Email);
+            var result = await _userManager.ChangeEmailAsync(user, data.Email, token);
 
             // Update user
-            var userUpdateResult = await _userManager.SetUserNameAsync(user, data.IdentityUser.Email.ToLower());
+            var userUpdateResult = await _userManager.SetUserNameAsync(user, data.Email.ToLower());
         }
 
         // Prepare user object
-        var appUser = data.ApplicationUser;
-        appUser.Id = data.IdentityUser.Id;
+        var appUser = new User()
+        {
+            Id = user.Id,
+            FirstName = data.FirstName,
+            LastName = data.LastName,
+            IsSuperAdmin = data.IsSuperAdmin
+        };
 
         await _repo.UpdateAsync(appUser);
 
-        return RedirectToAction("Edit", new { Id = data.IdentityUser.Id });
+        return RedirectToAction("Edit", new { Id = data.UserId });
     }
 
+    [ValidateAntiForgeryToken]
     [HttpDelete]
     public async Task<IActionResult> Delete(Guid? id)
     {
@@ -116,11 +149,5 @@ public class UsersController : Controller
         await _userManager.DeleteAsync(identityUser);
 
         return RedirectToAction("Index");
-    }
-
-    public class CombinedUser
-    {
-        public IdentityUser<Guid>? IdentityUser { get; set; }
-        public User? ApplicationUser { get; set; }
     }
 }
---
+++ src/OregonNexus.Broker.Web/Models/UserViewModel.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace OregonNexus.Broker.Web.Models;
+
+public class UserViewModel
+{
+    public IdentityUser<Guid>? IdentityUser { get; set; }
+
+    public Guid UserId { get; set; }
+
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; } = default!;
+
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; } = default!;
+
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email Address")]
+    public string Email { get; set; } = default!;
+
+    [Required]
+    [Display(Name = "Super Admin")]
+    public bool IsSuperAdmin { get; set; } = false;
+}
